d946f4ae6002ae1ad67cd8a0c8ad5065a86d21b5
Jonathan Payne <jonathan.payne1988@gmail.com>
2022-06-16 14:02:07 +0100

    Deleting rows

--- R/monthPlot.R
+++ R/monthPlot.R
@@ -1,6 +1,5 @@
 monthPlot.HydroAggs <- function(x, name = 'Gauge', polar = FALSE, snip = NULL, method = 'max', ...) {
   dt1 <- x$Monthly
-  #dt[, c('Year', 'Month') := tstrsplit(Year_Month, ' ', fixed=TRUE)]
   dt1$Year_Month <- gsub(' ', '-', dt1$Year_Month)
   dt1$Year_Month <- as.Date(paste(dt1$Year_Month,'-01',sep=''))
   colnames(dt1) <- c('Year_Month', 'Stat')
@@ -57,17 +56,3 @@ monthPlot <- function(x,...) {
 monthplot(Buildwas_Analysis, method = 'sum', name = 'Buildwas', polar = FALSE)
 monthplot(Buildwas_Analysis, name = 'Buildwas', polar = TRUE)
 monthPlot(Buildwas)
-
-dt1 <- Buildwas_Analysis$Monthly
-dt1$Year_Month <- gsub(' ', '-', dt1$Year_Month)
-dt1$Year_Month <- as.Date(paste(dt1$Year_Month,'-01',sep=''))
-colnames(dt1) <- c('Year_Month', 'Stat')
-dt1 <- headTail(dt1, n = snip)
-p <- ggplot(dt1, aes(x = month(Year_Month), y = Stat, group = year(Year_Month),colour = year(Year_Month))) +
-  geom_line(size = 1) +
-  xlab('Month') +
-  ggtitle(paste('Season plot of ', method, ' flows at ',  name, sep = '')) +
-  labs(colour = 'Year') +
-  scale_color_gradient(low = '#D2DE26', high = '#00A33B') +
-  scale_x_continuous(breaks = sort(unique(month(dt$Year_Month))), labels = month.abb) +
-  theme_light()
